866f724de62e3e7b8863ea6871aa87d946fd9dc5
Dilip <dilipkumar4813@gmail.com>
2017-05-17 08:35:42 +0530

    initial layout for the recipes

--- app/src/main/java/iamdilipkumar/com/udacitybaking/RecipesActivity.java
+++ app/src/main/java/iamdilipkumar/com/udacitybaking/RecipesActivity.java
@@ -2,12 +2,21 @@ package iamdilipkumar.com.udacitybaking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecipesActivity extends AppCompatActivity {
+
+    @BindView(R.id.gv_recipes)
+    GridView gridRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        ButterKnife.bind(this);
     }
 }
